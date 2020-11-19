1d918ae191ca64186adfefbd0d2f050a94649d09
NeroYuki <kingrfminecraft@gmail.com>
2020-11-20 00:32:39 +0700

    Add more BusinessLogic class

--- QLNH/BusinessLogicTier/YeuCauChinhSuaBUS.cs
+++ QLNH/BusinessLogicTier/YeuCauChinhSuaBUS.cs
@@ -2,10 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using DataModel;
+using DataAccessTier;
 
 namespace BusinessLogicTier
 {
     public class YeuCauChinhSuaBUS
     {
+        YeuCauChinhSuaDAO dataAccessObj = new YeuCauChinhSuaDAO();
+        public YeuCauChinhSuaHopDong GetYeuCauChinhSuaHopDong(string MaYC)
+        {
+            try
+            {
+                return dataAccessObj.GetYeuCauChinhSua(MaYC);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
     }
 }
